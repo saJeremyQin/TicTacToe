e241c60e922dcbf1b9631cb1efc2ac5ba3b67128
saJeremyQin <343220868@qq.com>
2022-11-10 16:41:06 +1030

    bot finished with current player bug

--- App.js
+++ App.js
@@ -1,48 +1,44 @@
-import React,{useState} from 'react';
+import React,{useState, useEffect} from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, ImageBackground, Pressable, Alert, LogBox } from 'react-native';
 import Cell from './components/cell';
 
-
 export default function App() {
   const [isXTurn, setIsXturn] = useState(true);
- 
   const [mapArray,setMapArray] = useState([
     ['','',''],
     ['','',''],
     ['','','']
   ]) ;
 
-  // onPress = (rowIndex, cellIndex)=>{
-  //   console.warn("Hello Lucien!", rowIndex, cellIndex)
-  // }
+  useEffect(() => {
+    if(!isXTurn)
+      botTurn();
+  },[isXTurn])
+
+  useEffect(() => {
+    const winner = getWinner(mapArray);
+    if(winner) {
+      gameWon(winner);
+    } else {
+      checkTieState();
+    }
+  },[mapArray])
+
   function onPress(rowIndex, columnIndex) {
-    // console.warn("Hello Lucien!", rowIndex, columnIndex);
-    if(mapArray[rowIndex][columnIndex] !== '')
-    {
+    if(mapArray[rowIndex][columnIndex] !== ''){
       return Alert.alert("Position already occupied!")
     }
   
-    // let cloneArray = JSON.parse(JSON.stringify(mapArray));
     let cloneArray = [...mapArray];
     cloneArray[rowIndex][columnIndex] = isXTurn ? 'x':'o';
     setMapArray(cloneArray);
-    // console.log(mapArray);
 
     // setMapArray((currentMapArray) => {
     //   let updateMapArray = [...currentMapArray];
     //   updateMapArray[rowIndex][columnIndex] = isXTurn ? 'x':'o';
     //   return updateMapArray;
     // })
-    // console.log(mapArray);
-
-    const winner = getWinner();
-    if(winner) {
-      gameWon();
-    } else {
-      checkTieState();
-    }
-     
     setIsXturn(!isXTurn);
   }
 
@@ -55,6 +51,50 @@ export default function App() {
     setIsXturn(true);
   }
 
+  const botTurn = () => {
+    // collect possbileOptions
+    const possbileOptions = [];
+    mapArray.forEach((row, rowIndex) => {
+      row.forEach((cell, cellIndex) =>{
+        if(cell == '')
+          possbileOptions.push({row:rowIndex,col:cellIndex})
+      })
+    })
+
+    let chosenOption;
+    // attack
+    possbileOptions.forEach((positionOption) => {
+      let cloneArray = JSON.parse(JSON.stringify(mapArray));
+      cloneArray[positionOption.row][positionOption.col] = 'o';
+      const winner = getWinner(cloneArray);
+
+      if(winner == 'o')
+        chosenOption = positionOption;  
+        if(!chosenOption) {
+          // defense strategy, check if there is a cell which could win
+          possbileOptions.forEach((positionOption) => {
+            let cloneArray = JSON.parse(JSON.stringify(mapArray));
+            cloneArray[positionOption.row][positionOption.col] = 'x';
+            const winner = getWinner(cloneArray);
+            // console.log("I am in possbileOptions");
+            if(winner == 'x')
+              chosenOption = positionOption   
+          })
+        }
+    })
+    // no possbility to win, give it a random value
+    if(!chosenOption) {
+      if(possbileOptions.length != 0) {
+        const ri = Math.floor(Math.random() * possbileOptions.length)
+        chosenOption = possbileOptions[ri];
+      }
+    }
+    // get a possiblity to win, then occupy it
+    if(chosenOption) {
+      onPress(chosenOption.row, chosenOption.col);
+    }
+  }
+
   const checkTieState = () => {
     if(!mapArray.some(row => row.some(cell => cell == '')))
       Alert.alert('It is a tie!', `tie`,[{
@@ -63,59 +103,46 @@ export default function App() {
       }])
   }
    
-
-  const gameWon = () => {
-    let player = isXTurn ? 'X':'O'
+  const gameWon = (winner) => {
+    // pass value here, and display who is the winner,better than judge here.
+    let player = winner=='x' ? 'X':'O'
     Alert.alert('Hurray!', `Player ${player} Won`,[{
       text:'Restart',
       onPress: resetGame
     }])
   }
 
-  const getWinner = () => {
-
+  const getWinner = (winnerMap) => {
     // same row to win
-
     for(let i = 0; i < 3; i++) {
-      // console.log("I am in row ",i);
       //The every() method returns true if the function returns true for all elements.
-      const isRowXWinning = mapArray[i].every((cell) => cell == 'x');
-      const isRowOWinning = mapArray[i].every((cell) => cell == 'o');
+      const isRowXWinning = winnerMap[i].every((cell) => cell == 'x');
+      const isRowOWinning = winnerMap[i].every((cell) => cell == 'o');
 
       if(isRowXWinning) {
-        // Alert.alert(`X won at row:${i} `);
-        // gameWon();
         return 'x';
       }
       if(isRowOWinning) {
-        // Alert.alert(`O won at row:${i} `);
-        // gameWon();
         return 'o';
       }
     }
 
     // same column to win
-  
     for(let col = 0; col < 3; col++){
 
       let isColXWinning = true;
       let isColOWinning = true;
       for(let row = 0; row < 3; row++) {
-        if(mapArray[row][col] != 'x')
+        if(winnerMap[row][col] != 'x')
           isColXWinning = false;
-        if(mapArray[row][col] != 'o')
+        if(winnerMap[row][col] != 'o')
           isColOWinning = false;
       }
       if(isColOWinning)
-        // Alert.alert(`O won at col:${col}`);
-        // gameWon();
         return 'o';
       if(isColXWinning)
-        // Alert.alert(`X won at col:${col}`);
-        // gameWon();
         return 'x';
     }
-
     // diagonal to win
     let isDiagXWinning = true;
     let isDiagOWinning = true;
@@ -123,25 +150,21 @@ export default function App() {
     let isDiagROWinning = true;
   
     for(let i = 0; i < 3; i++) {
-      if(mapArray[i][i] != 'x')
+      if(winnerMap[i][i] != 'x')
         isDiagXWinning = false;
-      if(mapArray[i][i] != 'o')
+      if(winnerMap[i][i] != 'o')
         isDiagOWinning = false;
 
 
-      if(mapArray[i][2-i] != 'x')
+      if(winnerMap[i][2-i] != 'x')
         isDiagRXWinning = false;
-      if(mapArray[i][2-i] != 'o')
+      if(winnerMap[i][2-i] != 'o')
         isDiagROWinning = false;  
     }
    
     if(isDiagOWinning || isDiagROWinning)
-      // Alert.alert(`O won at diag`);
-      // gameWon();
       return 'o';
     if(isDiagXWinning || isDiagRXWinning)
-      // Alert.alert(`X won at diag`);
-      // gameWon();
       return 'x';
   }
 
@@ -152,17 +175,8 @@ export default function App() {
     <View style={styles.container}>
       <ImageBackground 
         source={require("./assets/bg.jpeg")} 
-        resizeMode="contain"           // cover is the default one.
+        resizeMode="contain"                   // cover is the default one.
         style={styles.bgPic}> 
-
-        {/* <View style={styles.map}>
-          <View style={styles.circle}>
-          </View>
-         <View style={styles.cross}>
-            <View style={styles.crossLine}/>
-            <View style={[styles.crossLine,styles.crossLineNegative]}/>
-          </View>  
-        </View>  */}
      
         <Text style={{
           position:"absolute",
@@ -172,8 +186,7 @@ export default function App() {
           marginBottom:50}}>{textStr}</Text>
         <View style={styles.map}> 
         {
-          mapArray.map((row,rowIndex) => {
-            
+          mapArray.map((row,rowIndex) => {         
             return (
               <View 
                 key={`row-${rowIndex}`}       // `` is used when it is a template literal
@@ -211,9 +224,6 @@ const styles = StyleSheet.create({
     paddingTop:20 
   },
   map:{
-    // flex:1,
-    // borderWidth:1,
-    // borderColor:"red",
     width:"80%",
     aspectRatio:1
   },
